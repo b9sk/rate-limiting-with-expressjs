42c4efb4178e71d2243f36b2003f48a2f9e774db
HTML <i@b9sk.ru>
2025-02-18 22:03:51 +0300

    add ip handler

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import express from 'express';
 import { UserService } from './services/UserService';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.use(express.json());
+
+app.set('trust proxy', (ip: string) => {
+    if (ip === '127.0.0.1' || ip === '::1') return true // Localhost
+    if (ip.startsWith('172.') || ip.startsWith('10.') || ip.startsWith('192.168.')) {
+        return true // Trust Docker networks
+    }
+    return false
+})
 
 // Check if the server is running
 app.get('/login', (req, res) => {
@@ -17,7 +25,7 @@ app.post('/login', (req, res) => {
     res.send(req.body);
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
 
